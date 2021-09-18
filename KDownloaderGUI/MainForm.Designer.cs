9b7c2be256be1a8c432cd5241ec4c53abfb78148
PROUFUK52 <37846969+PROUFUK52@users.noreply.github.com>
2021-09-18 22:55:09 +0300

    Add files via upload
    this is code

---
+++ KDownloaderGUI/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * SharpDevelop tarafından düzenlendi.
+ * Kullanıcı: Ufcuk
+ * Tarih: 18.09.2021
+ * Zaman: 22:29
+ * 
+ * Bu şablonu değiştirmek için Araçlar | Seçenekler | Kodlama | Standart Başlıkları Düzenle 'yi kullanın.
+ */
+namespace KDownloaderGUI
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(534, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Dosya Linki";
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.button1.Location = new System.Drawing.Point(0, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(534, 40);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "İndir";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textBox2.Location = new System.Drawing.Point(0, 20);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(534, 20);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.Text = "Dosya Adı.exe";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(534, 78);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "KDownloader GUI";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ KDownloaderGUI/MainForm.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * SharpDevelop tarafından düzenlendi.
+ * Kullanıcı: Ufcuk
+ * Tarih: 18.09.2021
+ * Zaman: 22:29
+ * 
+ * Bu şablonu değiştirmek için Araçlar | Seçenekler | Kodlama | Standart Başlıkları Düzenle 'yi kullanın.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Windows.Forms;
+
+namespace KDownloaderGUI
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			using (var indirici = new WebClient())
+			{			
+				this.Text = "KDownloader GUI - İndirme İşlemi Sürüyor... | Coded by KaptanUfuk";
+				ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+				indirici.DownloadFile(textBox1.Text.ToString(), "C:\\Users\\"+Environment.UserName+"\\Desktop\\"+textBox2.Text.ToString());
+				this.Text = "KDownloader GUI - İndirme İşlemi Sonlandırıldı...";
+			}
+		}
+	}
+}
